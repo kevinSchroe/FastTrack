5acdfd9b6c032286c00084257668b0a47e25c69b
NinaFrost <nina.frost@t-online.de>
2018-07-17 14:56:06 +0200

    Anpassung der web.php

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,10 @@ Auth::routes();
 Route::get('/welcome', 'HomeController@index')->name('welcome');
 Route::get('/home', 'HomeController@home')->name('home');
 Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
+Route::get('/videos', 'HomeController@videos')->name('videos');
 Route::get('/admin_dashboard', 'AdminController@index')->name('admin_dashboard');
 
-Route::get('/example', 'ExampleController@index');
-Route::post('/example', 'ExampleController@example');
 
 Route::resource('/stammdaten', 'StammdatenController');
+Route::resource('/fragenkatalog', 'fragenkatalogController');
+Route::resource('/fahrlehrerVerwaltung', 'fahrlehrerVerwaltungController');
