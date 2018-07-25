9bf4be64d7ac07d8279bb03e37298a35cc9546e4
Kian1991 <kianluetke@gmail.com>
2018-07-25 10:53:42 +0200

    Neue SQL Datei hinzugefügt

---
+++ SQL/fasttrack_new_update.sql
@@ -0,0 +1,26 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Server Version:               10.1.26-MariaDB-0+deb9u1 - Debian 9.1
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             9.5.0.5249
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+-- Daten Export vom Benutzer nicht ausgewählt
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
