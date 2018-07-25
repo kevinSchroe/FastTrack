16b4c4b95e5d936a3c796075a184cbf753a67b9f
Kevin <kevin.schroeteler@edeka.de>
2018-07-25 16:01:31 +0200

    Bilder :D

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -14,7 +14,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="testfragen.edit">
+                            <a class="nav-link" href="fragenkatalog">
                                 <span data-feather="file"></span>
                                 Fragenverwaltung
                             </a>
