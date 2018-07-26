48a891c075afdccb020daf2db7781963fc6a4165
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-26 10:48:04 +0200

    Homeseite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,24 +26,25 @@
                 <div class="container-fluid bg-3 text-center">
 
                     <div class="row">
-                        <div class="col-sm-3">
+                        <div class="col-sm-13">
                             <h1>FLEXIBEL <br> </h1>
 
                             <p> Du kannst deine Fahrstunden bis 12 Stunden vorher kostenlos umbuchen</p>
                         </div>
-                        <div class="col-sm-3-w">
-                            <h1>EXZELLENTE AUSBILDUNG</h1>
+                        <div class="col-sm-13-w">
+                            <h1>KOMPETENT</h1>
                             <p class="card-text">Du fährst nur mit von uns geprüften und zertifizierten Fahrlehrern </p>
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-13">
                             <h1>GÜNSTIG</h1>
                             <p class="card-text">Du kannst zwischen verschiedenen Paketen wählen und hast die Kosten immer voll unter Kontrolle </p>
                         </div>
-                        <div class="col-sm-3-w">
-                            <h1>Unabhängig</h1>
-                            <p class="card-text">Absolviere deine Theoriewann und wo du willst, ganz einfach am Laptop oder Smartphone </p>
+                        <div class="col-sm-13-w">
+                            <h1>UNABHÄNGIG</h1>
+                            <p class="card-text">Absolviere deine Theorie wann und wo du willst, ganz einfach am Laptop oder Smartphone </p>
                         </div>
                     </div>
+
                 </div><br>
             </div>
 
