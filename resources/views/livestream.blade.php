f44511470619f8bb211a78bfaf16d8b1011ae5e3
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-26 16:32:00 +0200

    Livestream

---
+++ resources/views/livestream.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+@extends('layouts.app')
+@section('content')
+
+    <!--
+    Default-Anzeigeseite für den Livestream.
+    Der Livestream war keine Funktion, die im Ziel (Version 1) definiert wurde und wird lediglich durch eine Bilddatei symbolisch angedeutet.
+    -->
+
+<div class="container-fluid bg-3 text-center">
+        <div>
+            <img src="/FastTrack/resources/assets/img/warnmeldung.jpg">
+        </div>
+    </div>
+
+@endsection
