a8e856cf46bb2835266525a2de8627087f024053
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-28 00:49:49 +0200

    Videoseite kommentiert

--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <script>
-        function getId(url) { //Diese Funktion wird verwendet um normal youtube video URL's in 'embed urls' umzuwandeln
+        //Funktion verwandelt normale Youtubevideo URLs in "embed URLs"
+        function getId(url) {
             var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/; //Regular Expression
             var match = url.match(regExp);
 
@@ -13,13 +14,14 @@
             }
         }
     </script>
+
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Lernvideos</div>
 
-                    <div class="card-body">
+                        <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
@@ -31,23 +33,24 @@
                             ->select('video_url', 'video_title')
                             ->get();
                         ?>
-                        <div class="container">
 
-                            @foreach($videos as $video)
-                                <div><h3>{{$video->video_title}}</h3></div>
-                                <div id="iFrame{{$video->video_title}}"
-                                     style="margin-left: auto;margin-right: auto"></div>
-                                <script>
-                                    var videoId = getId('{{$video->video_url}}');
-                                    var iframeMarkup = '<iframe width="600" height="400" src="//www.youtube.com/embed/'
-                                        + videoId + '" frameborder="0" allowfullscreen></iframe>';
-                                    document.getElementById("iFrame{{$video->video_title}}").innerHTML = iframeMarkup;
-                                </script>
+                            <div class="container">
+                                <!-- alle Videos aus Datenbank ausgeben -->
+                                @foreach($videos as $video)
+                                    <div><h3>{{$video->video_title}}</h3></div>
+                                    <div id="iFrame{{$video->video_title}}"
+                                         style="margin-left: auto;margin-right: auto"></div>
+
+                                        <script>
+                                            var videoId = getId('{{$video->video_url}}');
+                                            var iframeMarkup = '<iframe width="600" height="400" src="//www.youtube.com/embed/'
+                                                 + videoId + '" frameborder="0" allowfullscreen></iframe>';
+                                                document.getElementById("iFrame{{$video->video_title}}").innerHTML = iframeMarkup;
+                                        </script>
                             @endforeach
 
-                    </div>
-                    </div>
-
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
