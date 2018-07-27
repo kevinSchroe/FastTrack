c2fbde4a04292edb6b685c788c22b3c6c2db99c4
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-28 00:55:10 +0200

    Werbeslogans kommentiert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,40 +3,35 @@
 
     <body>
         <div class="flex-center position-ref full-height">
-               <article>
-            <div class="container-fluid bg-3 text-center">
-
-                <div>
-                    <img src="/FastTrack/resources/assets/img/LogoFT.png">
-
-                </div>
-
+            <article>
                 <div class="container-fluid bg-3 text-center">
 
-                    <div class="row">
-                        <div class="col-sm-13">
-                            <h1>FLEXIBEL <br> </h1>
+                    <div><img src="/FastTrack/resources/assets/img/LogoFT.png"></div>
 
-                            <p> Du kannst deine Fahrstunden bis 12 Stunden vorher kostenlos umbuchen</p>
-                        </div>
-                        <div class="col-sm-13-w">
-                            <h1>KOMPETENT</h1>
-                            <p class="card-text">Du fährst nur mit von uns geprüften und zertifizierten Fahrlehrern</p>
-                        </div>
-                        <div class="col-sm-13">
-                            <h1>GÜNSTIG</h1>
-                            <p class="card-text">Du kannst zwischen verschiedenen Paketen wählen und hast die Kosten immer voll unter Kontrolle </p>
-                        </div>
-                        <div class="col-sm-13-w">
-                            <h1>UNABHÄNGIG</h1>
-                            <p class="card-text">Absolviere deine Theorie wann und wo du willst, ganz einfach am Laptop oder Smartphone </p>
-                        </div>
-                    </div>
+                        <div class="container-fluid bg-3 text-center">
+                            <!-- Werbeslogans -->
+                            <div class="row">
+                                <div class="col-sm-13">
+                                    <h1>FLEXIBEL</h1>
+                                    <p> Du kannst deine Fahrstunden bis 12 Stunden vorher kostenlos umbuchen</p>
+                                </div>
+                                <div class="col-sm-13-w">
+                                    <h1>KOMPETENT</h1>
+                                    <p class="card-text">Du fährst nur mit von uns geprüften und zertifizierten Fahrlehrern</p>
+                                </div>
+                                <div class="col-sm-13">
+                                    <h1>GÜNSTIG</h1>
+                                    <p class="card-text">Du kannst zwischen verschiedenen Paketen wählen und hast die Kosten immer voll unter Kontrolle </p>
+                                </div>
+                                <div class="col-sm-13-w">
+                                    <h1>UNABHÄNGIG</h1>
+                                    <p class="card-text">Absolviere deine Theorie wann und wo du willst, ganz einfach am Laptop oder Smartphone </p>
+                                </div>
+                            </div>
 
-                </div><br>
-            </div>
-
-    </article>
+                        </div>
+                </div>
+            </article>
         </div>
     </body>
 
