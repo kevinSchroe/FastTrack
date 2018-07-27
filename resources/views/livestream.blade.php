b490a0cdb1c256ae28e4f8b3e86b54a1d91070f4
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-28 00:56:13 +0200

    Livestream kommentiert

--- resources/views/livestream.blade.php
+++ resources/views/livestream.blade.php
@@ -4,13 +4,13 @@
 
     <!--
     Default-Anzeigeseite für den Livestream.
-    Der Livestream war keine Funktion, die im Ziel (Version 1) definiert wurde und wird lediglich durch eine Bilddatei symbolisch angedeutet.
+    Der Livestream war keine Funktion, die im Ziel definiert wurde und wird lediglich durch eine Bilddatei symbolisch angedeutet.
     -->
 
-<div class="container-fluid bg-3 text-center">
-        <div>
-            <img src="/FastTrack/resources/assets/img/warnmeldung.jpg">
-        </div>
+    <div class="container-fluid bg-3 text-center">
+
+        <div><img src="/FastTrack/resources/assets/img/warnmeldung.jpg"></div>
+
     </div>
 
 @endsection
