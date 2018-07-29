ef1a8f00e707e75d9549fe28a4401fdeb347fad9
Mirco <mdahms@rhrk.uni-kl.de>
2018-07-29 10:13:22 +0200

    Kommentiert und formatiert

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
+<!-- Errorseite, falls Fehler auftreten -->
+
 @section('content')
+
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -9,17 +12,15 @@
                     <div style="text-align: center;">
                         <img src="/FastTrack/resources/assets/img/ups.jpg">
 
-                    <h1 style="background-color: white"> <b> Uuups da ist was schief gelaufen…</b> </h1>
+                    <h1 style="background-color: white"> <b> Uuups, da ist wohl etwas schiefgelaufen…</b> </h1>
                     <h4 style="background-color: white">Diese Seite wurde leider nicht gefunden.</h4>
 
                         <div style="text-align: center">
                             <a href="{{ url('dashboard') }}" class="btn btn-success" role="button">Zurück zum Dashboard</a>
-
-
                         </div>
                     </div>
-
                 </div>
+             </div>
         </div>
     </div>
 @endsection
